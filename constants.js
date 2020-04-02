59822c859447a3fafeea8d05979f9abf403b6483
daniil-loban <dungeon_lda@mail.ru>
2020-04-02 07:56:33 +0300

    feat: add volume control

--- constants.js
+++ constants.js
@@ -3,9 +3,11 @@ const PLAYLISTS = 'playlists';
 const TRACKS = 'tracks';
 const LENGTH = 10;
 const AUDIO_PATH = './audio';
+const ADMIN_KEY = 'password';
 
 module.exports.DAYS = DAYS;
 module.exports.PLAYLISTS = PLAYLISTS;
 module.exports.TRACKS = TRACKS;
 module.exports.LENGTH = LENGTH;
 module.exports.AUDIO_PATH = AUDIO_PATH;
+module.exports.ADMIN_KEY = ADMIN_KEY;
